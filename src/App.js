0cbf3b3d03e63f204227521a1252f88c10d64d1f
Abdumannopov Abdulatif <abdulatif1215@gmail.com>
2024-01-14 18:54:57 +0500

    v-2.0.0

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <>
       <SectionWrapper 
-        title='You own store of Nifty NFTs. Start Selling & Growing'
-        description='Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef Marketplace.'
+        title='You own, store, sell, buy NFTs. Start Selling & Growing'
+        description='Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace.'
         showBtn
         mockupImg={assets.home_hero}
         banner='banner'
@@ -37,7 +37,7 @@ const App = () => {
         <p className={`${styles.pText} ${styles.whiteText}`}>
           Made with love by
           {" "}
-          <span className='font-black'>Abdulatif</span>
+          <span className='font-bold italic'>Abdulatif</span>
       </p>
       </div>
     </>
